b48b20e2843949995ada5735f716dae0171a51cd
Mehmet Sait Cubukcu <109916532+cubukcum@users.noreply.github.com>
2024-05-27 14:14:24 +0300

    fixed cpp issue

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -4,7 +4,7 @@
 #include "ndklogger.h"
 
 extern "C" JNIEXPORT jstring JNICALL
-Java_com_example_lane_1detection_1opencv_1kotlin_MainActivity_openCVVersion(JNIEnv *env, jobject thiz) {
+Java_com_example_lane_1detection_1kotlin_1opencv_MainActivity_openCVVersion(JNIEnv *env, jobject thiz) {
     std::string opencvVersion = cv::getVersionString();
     LOGI("OpenCV version: %s", opencvVersion.c_str());
     return env->NewStringUTF(opencvVersion.c_str());
